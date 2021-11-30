7048763ebfc0d16ebf3f94e0b78c610065eb46e5
Mayra Bruno <80061821+projetos-mayra@users.noreply.github.com>
2021-11-29 21:23:22 -0300

    Add files via upload

---
+++ exemplos_da_professora/javascript_entrada_dados/variaveis.js
@@ -0,0 +1,27 @@
+ // var x=2;
+  //var y=3.5;
+  //var z=7;
+  //var s1="estudo";
+  //var s2=" do ";
+  //var s3="JavaScript";
+function pedirValores(){
+		s1 = (prompt ("Digite a Campanha: "));
+		s2 = (prompt ("Informe seu nome:"));
+		s3 = parseInt (prompt ("Qual o Valor da sua compra: "));
+		//x = parseInt (prompt ("Digite um Valor para x:"));
+		//y = parseInt (prompt ("Dígite um valor para y:"));
+		//z = parseInt (prompt ("Digite um valor para z"));
+		pontos = (s3*2);
+
+
+//s=s1+" "+s2+" "+s3;         // concatenação de strings
+//soma=x+y;                   // adição
+//mult=x*y;                   // multiplicação
+//div=x/y;                    // divisão
+//sub=x-y;                    // subtracção
+//mod=x%y;                  // resto da divisão inteira
+
+
+}
+
+  
---
+++ exemplos_da_professora/javascript_passagem_parametro_retorno/calculo.js
@@ -0,0 +1,12 @@
+function soma(a,b) {
+return a+b;
+}
+function subtrair(a,b) {
+ return a - b;
+} 
+function mult(a,b) {
+ return a * b;
+}
+function div(a,b) {
+ return a * b;
+} 
